bec155724a9d058c5caabfab40782a9564e7b63b
권성준 <tjdwns1865@naver.com>
2022-12-04 02:41:45 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/programmers/baseballticketing/BaseballTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmers.baseballticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballTicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
